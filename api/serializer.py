402cb36dd97184375fc47d1186fbe9d074bd241e
Y0un9Ki <srilankakim66@gmail.com>
2024-03-21 02:15:49 +0900

    ADD : same username error alert add backend

--- api/serializer.py
+++ api/serializer.py
@@ -4,6 +4,7 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from .models import User
 
 User = get_user_model()
 
@@ -17,25 +18,31 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         return token
 
 class RegisterSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(required=True)
-    password = serializers.CharField(
-        write_only=True, required=True, validators=[validate_password])
+    # username = serializers.CharField(required=True)
+    # password = serializers.CharField(
+    #     write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
-    location = serializers.CharField(required=True)
-
+    # location = serializers.CharField(required=True)
+    # RegisterSerializer에서 ModelSerializer를 상속받았기에 model에서 가지고 있는 속성들을 알아서 가지고 온다. 
+    # 그렇기에 굳이 username, password, location을 정의를 안해줘도 되지만 헷갈리기에 그냥 정의하자!!!
     class Meta:
         model = User
         fields = ('username', 'password', 'password2', 'location')
+        # fields의 내가 model의 User의 password2를 정의해놓지 않았지만 RegisterSerializer에서 정의를 했기에 fields에 넣어주어야 한다.
 
     def validate(self, attrs): # ==> 여기서 attrs는 User의 저장된 username, password, location 키값에 들어가있는 value들을 딕셔너리 형태로 들고오는 것!!!
         if attrs['password'] != attrs['password2']: # ==> attrs['password']는 User의 password의 해당되는 value값 즉, 실제 비밀번호이다.  
             raise serializers.ValidationError(
                 {"password": 'not matched password1, password2'})
+        
+        if User.objects.filter(username=attrs['username']).exists():
+            raise serializers.ValidationError(
+                {"username": 'already username exist'})
 
         return attrs
 
     def create(self, validated_data):
-        location = validated_data.pop('location', None)
+        location = validated_data.pop('location', None) # None이 의미하는 것은 만약 location이 없으면 None값을 줘라라는 의미
         user = User.objects.create(
             username=validated_data['username'],
         )
