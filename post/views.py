a20aedc35d1450810a521ff37cd8da7f4ba50861
Y0un9Ki <srilankakim66@gmail.com>
2024-03-24 15:22:55 +0900

    COMPLETE : post app

--- post/views.py
+++ post/views.py
@@ -35,23 +35,13 @@ from rest_framework import status, viewsets
 #         serializer = PostSerializer(post)
 #         return Response(serializer.data)
     
-<<<<<<< HEAD
-    # def put(self, request, pk, format=None):
-    #     post = self.get_object(pk)
-    #     serializer = PostSerializer(post, data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-=======
-#     def put(self, request, pk, format=None):
-#         post = self.get_object(pk)
-#         serializer = PostSerializer(post, data=request.data)
-#         if serializer.is_valide():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
->>>>>>> back
+#    def put(self, request, pk, format=None):
+#       post = self.get_object(pk)
+#       serializer = PostSerializer(post, data=request.data)
+#       if serializer.is_valid():
+#           serializer.save()
+#           return Response(serializer.data)
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 #     def delete(self, request, pk, format=None):
 #         post = self.get_object(pk)
